a0ad15e81b7830adeb9047e1505bc0df52397909
Nivesh Rohankar <niveshrohankar@gmail.com>
2023-10-16 12:08:44 +0530

    changes

---
+++ src/pages/PostPage.js
@@ -0,0 +1,38 @@
+import { format } from "date-fns";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import {UserContext} from "../UserContext";
+
+export default function PostPage(){
+    const [postInfo, setPostInfo] = useState(null);
+    const {userInfo} = useContext(UserContext);
+    const {id} = useParams();
+    useEffect(() => {
+        console.log(id);
+        fetch(`http://localhost:4000/post/${id}`)
+        .then(response => {
+            response.json().then(postInfo => {
+                setPostInfo(postInfo);
+            });
+        });
+    }, []);
+
+    if(!postInfo) return '';
+
+    return(
+        <div className="post-page">
+            <h1>{postInfo.title}</h1>
+            <time>{format(new Date (postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
+            <div className="author">by @{postInfo.author.username}</div>
+            {userInfo.id === postInfo.author._id && (
+                <div className="edit-row">
+                    <Link className="edit-btn" to={`/edit/${postInfo._id}`}>Edit this post</Link>
+                </div>
+            )}
+            <div className="image">
+                <img src={`http://localhost:4000/${postInfo.cover}`} alt="Img"/>
+            </div>
+            <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+        </div>
+    );
+}
